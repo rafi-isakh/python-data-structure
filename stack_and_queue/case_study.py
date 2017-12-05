fa645f89b3bde55844cab5c83af8aade627aa42c
isakh <rafiisakh@gmail.com>
2017-12-05 14:09:15 +0700

    queue case study

--- stack_and_queue/case_study.py
+++ stack_and_queue/case_study.py
@@ -1,4 +1,7 @@
+from random import randint
+import time
 from stack import Stack
+from queue import Queue
 
 # Balance bracket checking
 def check_brackets(statement):
@@ -23,3 +26,22 @@ def check_brackets(statement):
         return False
     else:
         return True
+
+# Media player queue
+class Track:
+    def __init__(self, title=None):
+        self.title  = title
+        self.length = randint(5, 10)
+
+class MediaPlayerQueue(Queue):
+    def __init__(self):
+        super(MediaPlayerQueue, self).__init__()
+    
+    def add_track(self, track):
+        self.enqueue(track)
+    
+    def play(self):
+        while self.count > 0:
+            current_track = self.dequeue()  
+            print('Now playing {}'.format(current_track.data.title))
+            time.sleep(current_track.data.length)
